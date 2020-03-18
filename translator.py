d029327a736d0627a745201845557fbe308c65b2
Fox Larsson <fox@artfox.dev>
2020-03-18 09:07:13 +0300

    Implemented stage 4/7
    Added multilingual support for 13 languages:
                        1. Arabic
                        2. German
                        3. English
                        4. Spanish
                        5. French
                        6. Hebrew
                        7. Japanese
                        8. Dutch
                        9. Polish
                        10. Portuguese
                        11. Romanian
                        12. Russian
                        13. Turkish

--- translator.py
+++ translator.py
@@ -3,29 +3,51 @@ from bs4 import BeautifulSoup
 
 
 def choose_language():
-    prompt = 'Type "en" if you want to translate from French to English, or "fr" if you want to translate from ' \
-             'English into French:\n'
-    language = input(prompt)
-    return language
+    from_prompt = ''''Hello, you're welcome to the translator. Translator supports: 
+                    1. Arabic
+                    2. German
+                    3. English
+                    4. Spanish
+                    5. French
+                    6. Hebrew
+                    7. Japanese
+                    8. Dutch
+                    9. Polish
+                    10. Portuguese
+                    11. Romanian
+                    12. Russian
+                    13. Turkish
+                    Type the number of your language:\n'''
+    to_prompt = 'Type the number of language you want to translate to:\n'
+    from_code = int(input(from_prompt))
+    to_code = int(input(to_prompt))
+    language_key = {
+        1: 'Arabic',
+        2: 'German',
+        3: 'English',
+        4: 'Spanish',
+        5: 'French',
+        6: 'Hebrew',
+        7: 'Japanese',
+        8: 'Dutch',
+        9: 'Polish',
+        10: 'Portuguese',
+        11: 'Romanian',
+        12: 'Russian',
+        13: 'Turkish'
+    }
+    from_language = language_key[from_code]
+    to_language = language_key[to_code]
+    return from_language, to_language
 
 
 def choose_word():
-    word = input('Type the word you want to translate:\n')
+    word = input('Type the word you want to translate:\n').lower()
     return word
 
 
-def generate_url_info(target_language):
-    if target_language == 'en':
-        to_lang = 'english'
-        from_lang = 'fren
-    else:
-        to_lang = 'french'
-        from_lang = 'english'
-    return to_lang, from_lang
-
-
-def create_url(to_lang, from_lang, word):
-    url = f'https://context.reverso.net/translation/{from_lang}-{to_lang}/{word}'
+def create_url(from_lang, to_lang, word):
+    url = f'https://context.reverso.net/translation/{from_lang.lower()}-{to_lang.lower()}/{word}'
     return url
 
 
@@ -38,9 +60,8 @@ def check_connection(url):
         print('200 OK')
 
 
-def get_content(target_lang, user_word):
-    to_lang, from_lang = generate_url_info(target_lang)
-    url = create_url(to_lang, from_lang, user_word)
+def get_content(from_lang, to_lang, user_word):
+    url = create_url(from_lang, to_lang, user_word)
     user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:74.0) Gecko/20100101 Firefox/74.0'
     headers = {'User-Agent': user_agent}
     response = get_url(url, headers)
@@ -57,16 +78,12 @@ def parse_page(page):
     return translation_list[3:8], example_list[:10]
 
 
-def print_results(translation_list, example_list, language):
-    if language == 'en':
-        print_lang = 'English'
-    elif language == 'fr':
-        print_lang = 'French'
+def print_results(translation_list, example_list, to_lang):
     print('\nContext examples:')
-    print(f'\n{print_lang} Translations:')
+    print(f'\n{to_lang} Translations:')
     for word in translation_list:
         print(word)
-    print(f'\n{print_lang} Examples:')
+    print(f'\n{to_lang} Examples:')
     pairs = []
     for phrase in example_list:
         if example_list.index(phrase) < len(example_list) - 1 and example_list.index(phrase) % 2 == 0:
@@ -76,11 +93,11 @@ def print_results(translation_list, example_list, language):
 
 
 def main():
-    language = choose_language()
+    from_language, to_language = choose_language()
     word = choose_word()
-    page = get_content(language, word)
+    page = get_content(from_language, to_language, word)
     translations, examples = parse_page(page)
-    print_results(translations, examples, language)
+    print_results(translations, examples, to_language)
 
 
 main()
